0ac702f1dc3af9a0859d8812b3945a617089f731
MaiwennT <maiwenn.tanguy@outlook.com>
2022-07-06 15:32:18 +0200

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>amazona</NavBar.Brand>
+              </LinkContainer>
+            </Container>
+          </NavBar>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
